f872dae33f6404bb4026d08e1bfbeb278825d7ef
even-wei <evenwei@infuseai.io>
2024-12-06 17:09:26 +0800

    Upload DuckDB in CI and configure Codespace
    Signed-off-by: even-wei <evenwei@infuseai.io>

---
+++ .devcontainer/scripts/post_start.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Install dbt dependencies
+poetry run dbt deps
+
+# Download duckdb file
+gh repo set-default DataRecce/jaffle_shop_duckdb
+branch=$(git branch --show-current)
+run_id=$(gh run list --workflow "Recce CI" --branch $branch --status success --limit 1 --json databaseId --jq '.[0].databaseId')
+gh run download $run_id -n duckdb
+
+# Exec Recce
+poetry run recce server --cloud --review
